871fff13a1610c8901111c485fd92f9184115cc4
mochaa ♡ <68980781+mxchakitty@users.noreply.github.com>
2022-06-12 23:43:09 +0000

    forgot about the directory

--- dev.sh
+++ dev.sh
@@ -1,10 +1,11 @@
 # convenience script 
 SOURCE_FILE="main"
 EXECUTABLE_FILE="qtdemos"
+PROJECT_DIR="." 
 
 if [ "$1" = "test" ]; then
-    cd /home/geixcowo/db/QTDemos && qmake && make && rm .qmake.stash && rm $SOURCE_FILE.o && ./$EXECUTABLE_FILE
+    cd $PROJECT_DIR && qmake && make && rm .qmake.stash && rm $SOURCE_FILE.o && ./$EXECUTABLE_FILE
 elif [ "$1" = "build" ] || [ "$1" = "" ]; then
-    cd /home/geixcowo/db/QTDemos && qmake && make && rm .qmake.stash && rm $SOURCE_FILE.o
+    cd $PROJECT_DIR && qmake && make && rm .qmake.stash && rm $SOURCE_FILE.o
 fi
 
